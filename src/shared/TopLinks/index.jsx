7adf8ad383e752e78771aa9ed87723b952a86504
Jens Mouridtsen <jm@railmonitor.dk>
2019-06-28 13:45:58 +0200

    first deploy

---
+++ src/shared/TopLinks/index.jsx
@@ -0,0 +1,11 @@
+import React, { } from 'react';
+import './index.scss';
+
+function TopLinks() {
+    return (
+        <div className="topBarLinks">
+        </div>
+    );
+}
+
+export default TopLinks
